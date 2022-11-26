1ea7613fa4363ee03b301deeec15a62498ce98a5
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-11-26 09:27:35 +0530

    implement header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Header } from "../components";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Header />
       <main style={{ minHeight: "calc(100vh - 6rem)" }}>
         <Component {...pageProps} />;
       </main>
