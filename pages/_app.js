12ddebdd581228b6fff003d391f6d09d2506f2d0
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-11-26 09:17:01 +0530

    implement home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <main style={{ minHeight: "calc(100vh - 6rem)" }}>
+        <Component {...pageProps} />;
+      </main>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
